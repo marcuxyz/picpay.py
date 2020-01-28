1f4bdab75299a5869c973fce5948d52c22df2706
Marcus Pereira <mpereirassa@gmail.com>
2020-01-28 20:15:58 -0300

    create picpay class for request

--- python_picpay/__init__.py
+++ python_picpay/__init__.py
@@ -1,6 +1,26 @@
-__version__ = '1.0.0'
+__version__ = "1.0.0"
+
+import json
+from urllib.parse import urljoin
+
+from .download import Downloader
+
+PICPAY_DEFAULT_URL = "https://appws.picpay.com/ecommerce/public/"
 
 
 class Picpay:
-    def get():
-        pass
+    def __init__(self, token: str):
+        self.token = token
+        self.headers = {
+            "Content-Type": "application/json",
+            "x-picpay-token": self.token,
+        }
+
+    def payment(self, payment_data: dict):
+        payment_url = urljoin(PICPAY_DEFAULT_URL, "payments")
+
+        response = Downloader().post(
+            payment_url, data=json.dumps(payment_data), headers=self.headers
+        )
+
+        return response
