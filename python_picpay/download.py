341137ee01241c4c53254bcbad0a1a8d8a521203
Marcus Pereira <mpereirassa@gmail.com>
2020-01-29 00:55:55 -0300

    fix download certificate

--- python_picpay/download.py
+++ python_picpay/download.py
@@ -7,7 +7,7 @@ class Downloader:
 
     def get(self, url, params=None, cookies=None, headers=None):
         return self.session.get(
-            url, params=params, verify=False, cookies=cookies, headers=headers
+            url, params=params, cookies=cookies, headers=headers
         )
 
     def post(self, url, data=None, params=None, cookies=None, headers=None):
@@ -15,7 +15,6 @@ class Downloader:
             url,
             data=data,
             params=params,
-            verify=True,
             headers=headers,
             cookies=cookies,
         )
