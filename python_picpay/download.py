1b60d06b6aed1ce7bc56af18846761e25436d23a
Marcus Pereira <mpereirassa@gmail.com>
2020-01-29 00:58:56 -0300

    format

--- python_picpay/download.py
+++ python_picpay/download.py
@@ -6,17 +6,11 @@ class Downloader:
         self.session = requests.Session()
 
     def get(self, url, params=None, cookies=None, headers=None):
-        return self.session.get(
-            url, params=params, cookies=cookies, headers=headers
-        )
+        return self.session.get(url, params=params, cookies=cookies, headers=headers)
 
     def post(self, url, data=None, params=None, cookies=None, headers=None):
         return self.session.post(
-            url,
-            data=data,
-            params=params,
-            headers=headers,
-            cookies=cookies,
+            url, data=data, params=params, headers=headers, cookies=cookies,
         )
 
     def close(self):
