ecbfe48f7d9eabc949894ce2bb88dd425bfde42c
Marcus Pereira <mpereirassa@gmail.com>
2020-03-30 16:27:46 -0300

    implement async requests #9

--- picpay/download.py
+++ picpay/download.py
@@ -1,17 +1,23 @@
-import requests
+import httpx
+from httpx import Response
 
 
 class Downloader:
-    def __init__(self):
-        self.session = requests.Session()
+    def __init__(self, headers=None):
+        self.sess_async = httpx.AsyncClient(headers=headers)
+        self.sess = httpx.Client(headers=headers)
 
-    def get(self, url, params=None, cookies=None, headers=None):
-        return self.session.get(url, params=params, cookies=cookies, headers=headers)
+    def get(self, url, params=None, headers=None) -> Response:
+        return self.sess.get(url, params=params)
 
-    def post(self, url, data=None, params=None, cookies=None, headers=None):
-        return self.session.post(
-            url, data=data, params=params, headers=headers, cookies=cookies,
-        )
+    def post(self, url, data=None, params=None, headers=None) -> Response:
+        return self.sess.post(url, data=data, params=params)
 
-    def close(self):
-        self.session.close()
+    async def get_async(self, url, params=None, headers=None) -> Response:
+        response = await self.sess_async.get(url, params=params)
+        return response
+
+    async def post_async(self, url, data=None, params=None, headers=None) -> Response:
+        response = await self.sess_async.post(url, data=data, params=params)
+
+        return response
